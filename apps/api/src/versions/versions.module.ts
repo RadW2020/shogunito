6c04a7ea52a1f202aea5291a3ad2f5fd5d9b58df
RadW2020 <eclarieurnoir@gmail.com>
2026-01-11 00:49:15 +0100

    import versions

--- apps/api/src/versions/versions.module.ts
+++ apps/api/src/versions/versions.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { VersionsService } from './versions.service';
 import { VersionsController } from './versions.controller';
 import { FilesModule } from '../files/files.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { ProjectAccessModule } from '../auth/services/project-access.module';
+import { Version } from '../entities/version.entity';
+import { Asset } from '../entities/asset.entity';
+import { Sequence } from '../entities/sequence.entity';
+import { Status } from '../entities/status.entity';
 
 @Module({
-  imports: [FilesModule, NotificationsModule, ProjectAccessModule],
+  imports: [
+    TypeOrmModule.forFeature([Version, Asset, Sequence, Status]),
+    FilesModule,
+    NotificationsModule,
+    ProjectAccessModule,
+  ],
   controllers: [VersionsController],
   providers: [VersionsService],
   exports: [VersionsService],
